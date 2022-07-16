c7dd317103587012ef7e7a5e20f8e1555af038be
杨杨 <2979742951@qq.com>
2022-07-16 15:28:15 +0800

    组件

---
+++ WebCoding/demo-first/src/Test.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="text-box">
+        <h3>这是自定义的组件{{userName}}</h3>
+        
+    </div>
+</template>
+
+
+<script>
+    export default{
+        data(){
+            return {
+                userName: '张三',
+            }
+        }
+    }
+</script>
+
+<style>
+.text-box {
+    background-color: pink;
+}
+
+
+</style>
---
+++ WebCoding/demo-first/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+// import App from './App.vue'
+import Test from './Test.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+
+  render: h => h(Test),
+  
+}).$mount('#app')
